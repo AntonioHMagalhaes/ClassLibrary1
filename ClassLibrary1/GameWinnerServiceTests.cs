2ac177caec0f4b282832f4041cc5862894843ec7
N4G4TO <N4G4TO@DESKTOP-TSTL4NM>
2016-10-14 09:42:37 +0200

    Add project files.

---
+++ ClassLibrary1/GameWinnerServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToe.Services;
+
+namespace TicTacToe
+{
+    public class GameWinnerServiceTests
+    {
+        public void NeitherPlayerHasThreeInARow()
+        {
+            IGameWinnerService gameWinnerService;
+            gameWinnerService = new GameWinnerService();
+            const char expected = ' ';
+            var gameBoard = new char[3, 3] {{' ', ' ', ' '},
+                                            {' ', ' ', ' '},
+                                            {' ', ' ', ' '} };
+            var actual = gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void PlayerWithAllSpacesInTopRowIsWinner()
+        {
+            IGameWinnerService gameWinnerService;
+            gameWinnerService = new GameWinnerService();
+            const char expected = 'X';
+            var gameBoard = new char[3, 3]
+            { {expected, expected, expected},
+               {' ', ' ', ' '},
+               {' ', ' ', ' '} };
+            var actual = gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected.ToString(),
+                actual.ToString());
+
+            }
+        }
+    }
+
---
+++ TicTacToe.UnitTestsTests/GameWinnerServiceTestsTests.cs
@@ -0,0 +1,74 @@
+﻿
+using TicTacToe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToe.Services;
+using NUnit.Framework;
+
+namespace TicTacToe.Tests
+{
+    [TestFixture]
+    public class GameWinnerServiceTestsTests
+    {
+        IGameWinnerService _gameWinnerService;
+        private char[,] _gameBoard;
+        
+        [SetUp]
+        public void SetupUnitTests()
+        {
+            _gameWinnerService = new GameWinnerService();
+            _gameBoard = new char[3, 3] {{' ', ' ', ' '},
+                                            {' ', ' ', ' '},
+                                            {' ', ' ', ' '} };
+        }
+
+        [Test]
+        public void NeitherPlayerHasThreeInARowTest()
+        {
+
+            const char expected = ' ';
+            var actual = _gameWinnerService.Validate(_gameBoard);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void PlayerWithAllSpacesInTopRowIsWinnerTest()
+        {
+
+            const char expected = 'X';
+            for (var rowIndex = 0; rowIndex < 3; rowIndex++)
+            {
+                _gameBoard[0, rowIndex] = expected;
+            }
+            var actual = _gameWinnerService.Validate(_gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+        [Test]
+        public void PlayerWithAllSpacesInFirstColumnIsWinner()
+        {
+            const char expected = 'X';
+            for (var columnIndex = 0; columnIndex < 3; columnIndex++)
+            {
+                _gameBoard[columnIndex, 0] = expected;
+            }
+            var actual = _gameWinnerService.Validate(_gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+
+        [Test]
+
+        public void PlayerWithThreeInARowDiagonallyDownAndToRightIsWinner()
+        {
+            const char expected = 'X';
+            for (var cellIndex = 0; cellIndex < 3; cellIndex++)
+            {
+                _gameBoard[cellIndex, cellIndex] = expected;
+            }
+            var actual = _gameWinnerService.Validate(_gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+    }
+}
